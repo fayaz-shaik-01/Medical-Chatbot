1349beef566c1f01ba4fd5684919a82ca36697a3
fayaz-shaik-01 <shaikfayaz0701@gmail.com>
2025-10-19 21:33:28 +0530

    Update the Project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '2.0.0',
+    author= 'Fayaz Shaik',
+    author_email= 'shaikfayaz0701@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
